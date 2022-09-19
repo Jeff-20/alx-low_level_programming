c747dbbd128ae44c24337b9562ce96779d840211
Jeff-20 <maribetshepomakwela@gmail.com>
2022-09-19 15:44:31 -0700

    Rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string,
+ * followed by a new line
+ * @s: an input string
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (len)
+
+_putchar(s[--len]);
+
+_putchar('\n');
+}
