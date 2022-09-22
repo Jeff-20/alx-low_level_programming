66a1f16d96199fe3ec7c3c1118bde73ee675b4ef
Jeff-20 <maribetshepomakwela@gmail.com>
2022-09-22 05:42:14 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+while (strcmp(s1, s2))
+{
+
+if (*s1 != *s2)
+
+return (*s1 - *s2);
+}
+return (*s1 - *s2);
+
+}
