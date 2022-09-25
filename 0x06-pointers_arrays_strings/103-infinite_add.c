24311fa1577df3262b0c2666db2cf922748ec224
Jeff-20 <maribetshepomakwela@gmail.com>
2022-09-25 14:26:53 -0700

    ROT13

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <math>
+/**
+ * infinite_add - a function that adds two numbers
+ * @n1: input string
+ * @n2: input string
+ * @r: A buffer to store the result
+ * size_r: size of r
+ * Return: returns the pointer to the result
+ * or 0 if the result cannot be stored in r
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+{
+add(n1, n2);
+
+_putchar("n1 + n2 = r");
+
+return (*r);
+
+}
