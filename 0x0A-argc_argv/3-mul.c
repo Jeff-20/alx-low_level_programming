8af625b6a00a2ffbeedd3d37cebc166c03e449dc
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-01 14:29:24 -0700

    Mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,21 +13,6 @@ int main(int argc, char **argv)
 {
 
 int n1, n2, res;
-int i;
-
-for (i = 1; i < argc; i++)
-
-{
-n1 = atoi(argv[i]);
-n2 = atoi(argv[i + 1]);
-res = n1 *n2;
-
-printf("%d\n", res);
-
-return (0);
-
-}
-
 
 if (argc != 3)
 
@@ -38,6 +23,17 @@ return (1);
 
 }
 
+else
+
+{
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+res = n1 *n2;
+
+printf("%d\n", res);
+
 return (0);
+
+}
 
 }
