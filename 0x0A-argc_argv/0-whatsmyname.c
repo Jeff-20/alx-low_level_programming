6da0b77e718911001999f51e73c5de193d8fd9f6
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-01 05:03:51 -0700

    Mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(int argc, char* argv[])
+
+{
+
+int i = 0;
+
+for (;i < argc; i++)
+
+{
+
+printf("%s\n", argv[i]);
+
+}
+
+return (0);
+
+}
