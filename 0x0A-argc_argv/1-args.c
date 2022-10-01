5776ff2a0b250b39a4562d182d560ada0b70894e
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-01 06:11:24 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: array of pointers of arrays of characters
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+
+return (0);
+
+}
