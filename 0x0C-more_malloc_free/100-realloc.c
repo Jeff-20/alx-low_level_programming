b63d975d1f42eeb768f430ec2cd7bd39f40e1772
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-09 05:52:29 -0700

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,11 +12,11 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *ptr_new;
+int *ptr_new;
 
-ptr = (char *) malloc(old_size);
+ptr = malloc(old_size);
 
-ptr_new = (char *) malloc(new_size);
+ptr_new = malloc(new_size);
 
 memcpy(ptr_new, ptr, old_size);
 
