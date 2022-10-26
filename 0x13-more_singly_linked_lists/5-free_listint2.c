3700154a423296371ee934897bd0db5d7a9fb2a3
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-26 09:36:27 -0700

    5. list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,10 +15,14 @@ void free_listint2(listint_t **head)
 listint_t *tmp;
 tmp = *head;
 
+if (head == NULL)
+return;
+
 while (tmp != NULL)
 {
 tmp = tmp->next;
 free(*head);
 (*head) = tmp;
 }
+*head = NULL;
 }
