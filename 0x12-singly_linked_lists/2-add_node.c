fe424db5b779fb3b8049556091342d619df25c63
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-30 03:56:28 -0700

    2.add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <unistd.h>
 
 /**
 * add_node - function that adds a new node at the beginning
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* add_node_end - adds a new node at the end of
+* a list_t list
+* @head: pointer to first node
+* @str: string to be printed
+* Return: address of the new element, or NULL if failed
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *tmp;
+list_t *new;
+
+new = malloc(sizeof(list_t));
+
+if (new == NULL)
+return (NULL);
+
+new->str = strdup(str);
+new->next = NULL;
+
+if (*head == NULL)
+*head = new;
+
+else
+{
+tmp = *head;
+
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+
+}
+return (new);
+}
