810cb9e9a0f2fece4184b0f25082f587193bdfff
Jeff-20 <maribetshepomakwela@gmail.com>
2022-10-30 11:27:23 -0700

    4.free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_listint - frees a listint_t list
+* @head: pointer to the first node of the list
+*
+* Return: Nothing
+*/
+
+void free_list(list_t *head)
+{
+list_t *f;
+
+f = head;
+
+while (head)
+{
+f = head;
+
+head = head->next;
+free(f->str);
+free(f);
+}
+free(head);
+}
