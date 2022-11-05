6018c235fe4b04daa76436b72375eb9e5dbc5c3f
Jeff-20 <maribetshepomakwela@gmail.com>
2022-11-04 19:18:34 -0700

    100.ELF

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,7 +6,7 @@
  * @argv: argument vector
  * Return: 1 on success, status code 98 on failure
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 printf("argc:%d, argv:%p\n", argc, (void *)*argv);
 
